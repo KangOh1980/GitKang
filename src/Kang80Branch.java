4cdfe453d0c5026f581578bfc6dac3a14f329d9e
KangOh1980 <dmcddungc@gmail.com>
2023-01-09 10:40:48 -0500

    2nd Commit from Kang Branch

--- src/Kang80Branch.java
+++ src/Kang80Branch.java
@@ -1,5 +1,6 @@
 public class Kang80Branch {
     public static void main(String[] args) {
         System.out.println("From Kang Branch");
+        System.out.println("Fromd 2nd Kang Branch");
     }
 }
--- src/Yo.java
+++ src/Yo.java
@@ -3,6 +3,6 @@ public class Yo {
         System.out.println("Hello");
         System.out.println("Yello");
         System.out.println("Hello again");
-        System.out.println("again");
+
     }
 }
